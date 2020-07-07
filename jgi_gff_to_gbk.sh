d20c760d279f168adfad72ab39582789f0f14ec7
Lukas Jelonek <lukas.jelonek@computational.bio.uni-giessen.de>
2020-07-07 11:31:01 +0200

    Finish script to convert a jgi genome download (fas.gz, gff.gz. kegg.tsv.gz) to a genbank file

--- jgi_gff_to_gbk.sh
+++ jgi_gff_to_gbk.sh
@@ -6,20 +6,39 @@ set -e
 
 GFF=$1
 FASTA=$2
+KEGG_ANNOTATION=$3
+ORGANISM_NAME=$4
+STRAIN=$5
 
-PREFIX=${GFF%%.*}
+PREFIX=${GFF##*/}
+PREFIX=${PREFIX%%.*}
 
 echo File prefix: $PREFIX
 
 tmp_dir=$(mktemp -d -t jgi2gbk-XXXXXXXXXX)
 
-zcat $GFF > $tmp_dir/jgi.gff
+function finish {
+    echo Cleanup
+    rm -rf $tmp_dir
+}
+
+trap finish EXIT
+
+echo Extracting fasta
 zcat $FASTA > $tmp_dir/seq.fas
 
-perl ~/Projects/gfftools/jgi2gff.pl $tmp_dir/jgi.gff
-~/tmp/genometools-1.6.1/bin/gt gff3 -sort -tidy -addids no $tmp_dir/jgi.gff.gff > $tmp_dir/gt.gff3
+echo Convert to gff3
+~/Projects/gfftools/jgi2gff3.py $GFF > $tmp_dir/jgi.gff
+echo Sort and cleanup gff3
+~/tmp/genometools-1.6.1/bin/gt gff3 -sort -tidy -addids no -retainids $tmp_dir/jgi.gff > $tmp_dir/gt.gff3
+echo Add sequence-regions
 ~/Projects/gfftools/fix_gendbe_gff.py --no_fix_cds_names --no_fix_escapes_in_attributes --no_include_fasta --gff $tmp_dir/gt.gff3 --fasta $tmp_dir/seq.fas > $tmp_dir/fixed.gff3
+echo Add Kegg annotation
+~/Projects/gfftools/add_jgi_annotation_to_gff.py --gff $tmp_dir/fixed.gff3 --annotation $KEGG_ANNOTATION > $tmp_dir/annotation.gff3
 
-~/Projects/gfftools/gff3_to_gbk.py $tmp_dir/fixed.gff3 $tmp_dir/seq.fas
-mv $tmp_dir/fixed.gb $PREFIX.gb
-rm -rf $tmp_dir
+echo Convert to gbk
+~/Projects/gfftools/gff3_to_gbk.py $tmp_dir/annotation.gff3 $tmp_dir/seq.fas "$ORGANISM_NAME" "$STRAIN"
+
+mv $tmp_dir/annotation.gb $PREFIX.gb
+mv $tmp_dir/annotation.gff3 $PREFIX.gff3
+
